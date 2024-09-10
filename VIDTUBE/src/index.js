d27d3d404157f1ca9b029d58d1935df8ee272feb
Adesh <121770101+Adeshraigit@users.noreply.github.com>
2024-09-10 23:14:40 +0530

    changes

--- VIDTUBE/src/index.js
+++ VIDTUBE/src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: "./.env",
+});
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} 15:36`);
+});
