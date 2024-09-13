8f81ca31ac67fb5c203e95bf2466c57c08925b75
Adesh <121770101+Adeshraigit@users.noreply.github.com>
2024-09-13 19:03:49 +0530

    changes

--- VIDTUBE/src/app.js
+++ VIDTUBE/src/app.js
@@ -10,6 +10,7 @@ app.use(
     })
 )
 
+// common middlewares
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({limit: "16kb", extended: true, }));
 app.use(express.static("public"));
--- VIDTUBE/src/index.js
+++ VIDTUBE/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
+import { connectDB } from "./db/index.js";
 
 dotenv.config({
     path: "./.env",
@@ -7,6 +8,13 @@ dotenv.config({
 
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} 15:36`);
-});
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+})
+.catch((err) => {
+    console.log("MongoDB connection error", err);
+})
