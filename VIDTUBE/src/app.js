bdda900688d34638618438fbe6d7964ba2a34e6d
Adesh <121770101+Adeshraigit@users.noreply.github.com>
2024-09-15 19:22:08 +0530

    healthcheck

--- VIDTUBE/src/app.js
+++ VIDTUBE/src/app.js
@@ -15,6 +15,11 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({limit: "16kb", extended: true, }));
 app.use(express.static("public"));
 
+// import routes
+import { healthcheck } from "./controllers/healthcheck.controllers.js";
+
+// routes
+app.use("/api/v1/healthcheck", healthcheck);
 
 
 export { app };
